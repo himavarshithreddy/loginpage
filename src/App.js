26da25c408c4bcc2fb78f766d306586a355d011f
Himavarshith Reddy <himavarshithreddy@gmail.com>
2023-11-19 02:57:34 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useRef } from 'react';
+import LoadingBar from 'react-top-loading-bar';
 
 function App() {
+ const [action,setaction]=useState("Sign In");
+ const loadingBar = useRef(null);
+
+  const handleToggle = () => {
+    loadingBar.current.continuousStart();
+    loadingBar.current.complete(); // Start the loading bar animation
+    
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div class="triangle1"></div>
+     <div class="triangle2"></div>
+     <div class="triangle3"></div>
+     <div class="triangle4"></div>
+     <div className='container'>
+      <div className='header'>
+     <div className='text'>{action}</div>
+     <div className='underline'></div>
+     </div>
+     <div className='toggle'>
+      <button className={action==="Sign In"?"btn btn1 active" : "btn btn1"} onClick={()=>{setaction("Sign In"); handleToggle()} }>Sign In</button>
+      <button className={action==="Sign Up"?"btn btn2 active" : "btn btn2"} onClick={()=>{setaction("Sign Up"); handleToggle()}}>Sign Up</button>
+     </div>
+     <LoadingBar color='#91d223' ref={loadingBar} />
+     </div>
     </div>
   );
 }
