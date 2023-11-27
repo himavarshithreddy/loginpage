b512df04341d5c4a82e1fec7cd228cdbc75fd8d7
Himavarshith Reddy <himavarshithreddy@gmail.com>
2023-11-27 06:58:29 +0530

    res

--- src/dashboard.js
+++ src/dashboard.js
@@ -165,10 +165,10 @@ navigate("/");
 
     return(<div className="dashboard">
      
-<div className="triangle1"></div>
-     <div className="triangle2"></div>
-     <div className="triangle3"></div>
-     <div className="triangle4"></div>
+<div className="dtriangle1"></div>
+     <div className="dtriangle2"></div>
+     <div className="dtriangle3"></div>
+     <div className="dtriangle4"></div>
      <div className="con">
       <div id='recaptcha-container'></div>
      <div className='dheader'>
